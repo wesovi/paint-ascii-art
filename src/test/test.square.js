0e82daac9a0652f8b4502e584dafbbbf2d0ddebe
Nicolas Quiceno <nquicenob@gmail.com>
2016-03-13 23:46:31 +0100

    testing files

---
+++ src/test/test.square.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var expect = chai.expect;
+
+var should = chai.should();
+
+describe('Create a square.',function(){
+
+    it('create square with default values', function() {
+
+    });
+
+    it('create rectangle with various sizes', function() {
+
+    });
+
+    it('create rectangle with various degree tilt', function() {
+
+    });
+
+});
