0fab9bb8c99dcc57169b33dde09235175f97640d
Nicolas Quiceno <nquicenob@gmail.com>
2016-03-15 01:00:09 +0100

    quadrilateral test in green

--- src/lib/quadrilateral.js
+++ src/lib/quadrilateral.js
@@ -1,14 +1,34 @@
-function createQuadrilateral(valuesToCreate){
-  valuesToCreate = valuesToCreate || {};
+"use strict";
+
+function create(valuesToCreate = {}){
+
   const valuesToBuild = Object.assign({
-    width : 2,
-    height : 2,
-    size : 1,
+    width : 1,
+    height : 1,
+    size : 2,
     degree : 0
   }, valuesToCreate);
 
+  function areNumbersTheValues(values) {
+    return !Object.keys(values).some(function(value){
+      return Number.isNaN(+values[value]);
+    });
+  }
+
+  function getValidDegree(degree) {
+    const sign = degree > 0 ? +1 : -1;
+    degree = Math.abs(degree);
+    return sign * (function recursiveDegree(degreeR){
+      return degreeR >= 360 ? recursiveDegree((degreeR - 360)) : degreeR;
+    })(degree);
+  }
+
   const build = function build() {
-    const degree =  valuesToBuild.degree;
+    areNumbersTheValues(valuesToBuild) || function(){
+      throw new TypeError('value to build is nan');
+    }();
+
+    const degree = getValidDegree(valuesToBuild.degree);
     const content = Array(valuesToBuild.height * valuesToBuild.size)
       .fill('M'.repeat(valuesToBuild.width * valuesToBuild.size));
 
@@ -39,5 +59,5 @@ function createQuadrilateral(valuesToCreate){
 }
 
 module.exports = {
-  create : createQuadrilateral
+  create
 };
