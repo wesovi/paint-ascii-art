f4cb675d23874596910a2667a40864341ef9e597
Iván Corrales <developer@wesovi.com>
2016-03-16 21:35:46 +0100

    feat: adding eslint configuration and fixing some issues

--- src/lib/quadrilateral.js
+++ src/lib/quadrilateral.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
-const getBuilderData = function getBuilderData(valuesToCreate){
+const getBuilderData = function getBuilderData(valuesToCreate) {
   return Object.assign({
-    width : 1,
-    height : 1,
-    size : 2,
-    degree : 0
+    width: 1,
+    height: 1,
+    size: 2,
+    degree: 0,
   }, valuesToCreate);
 };
 
@@ -14,49 +14,49 @@ const areNumbersTheValues = function areNumbersTheValues(valuesToCheck) {
 };
 
 const getValidDegree = function getValidDegree(degree) {
-    return degree % 360;
+  return degree % 360;
 };
 
-const multValsAndRound = function multValsAndRound(...vals){
+const multValsAndRound = function multValsAndRound(...vals) {
   return Math.round(vals.reduce((total, elem) => total * elem));
 };
 
-const getArrayContent = function getArrayContent(size, height, width){
+const getArrayContent = function getArrayContent(size, height, width) {
   return Array(multValsAndRound(height, size))
     .fill(Array(multValsAndRound(width, size)).fill('M'));
 };
 
 const setValueInBuilderFn = function setValueFn(key, builderData) {
-  return function(value){
-    builderData[key] = value;
+  return function _(value) {
+    builderData[key]= value;
     return this;
   };
 };
 
-const getBuilderFn = function getBuilderFn(builderData){
+const getBuilderFn = function getBuilderFn(builderData) {
   return function build() {
-    const {size, height, width, degree}  = builderData;
-    areNumbersTheValues([size, height, width, degree]) || function(){
-      throw new TypeError('value to build is nan');
+    const { size, height, width, degree }= builderData;
+    areNumbersTheValues ([size, height, width, degree]) || function() {
+      throw new TypeError ('value to build is nan');
     }();
 
     return {
       content : getArrayContent(size, height, width),
-      degree : getValidDegree(degree)
+      degree : getValidDegree(degree),
     };
-  }
+  };
 };
 
 module.exports = {
-  create : function create(valuesToCreate = {}){
+  create: function create(valuesToCreate = {}) {
     const builderData = getBuilderData(valuesToCreate);
 
     return Object.freeze({
-      build  : getBuilderFn(builderData),
-      width  : setValueInBuilderFn('width', builderData),
-      height : setValueInBuilderFn('height', builderData),
-      size   : setValueInBuilderFn('size', builderData),
-      degree : setValueInBuilderFn('degree', builderData)
+      build: getBuilderFn(builderData),
+      width: setValueInBuilderFn('width', builderData),
+      height: setValueInBuilderFn('height', builderData),
+      size: setValueInBuilderFn('size', builderData),
+      degree: setValueInBuilderFn('degree', builderData),
     });
-  }
+  },
 };
